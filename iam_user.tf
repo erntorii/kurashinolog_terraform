3a5d5f88fd01943feb0431c0980dd3df7b745162
erntorii <ern4273@gmail.com>
2020-10-16 19:25:06 +0900

    Fix deploy-user attach policy

--- iam_user.tf
+++ iam_user.tf
@@ -2,16 +2,16 @@ resource "aws_iam_user" "deploy-user" {
   name = "${var.prefix}-deploy-user"
 }
 
-data "aws_iam_policy" "ecr-power-user" {
-  arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
-}
-
-resource "aws_iam_policy" "deploy" {
-  name   = "${var.prefix}-deploy"
-  policy = data.aws_iam_policy.ecr-power-user.policy
+variable "deploy_policies" {
+  type = list(string)
+  default = [
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerServiceFullAccess"
+  ]
 }
 
 resource "aws_iam_user_policy_attachment" "deploy-attach" {
   user       = aws_iam_user.deploy-user.name
-  policy_arn = aws_iam_policy.deploy.arn
+  count      = length(var.deploy_policies)
+  policy_arn = var.deploy_policies[count.index]
 }
